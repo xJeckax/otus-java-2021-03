8843d5f79a92d692b2c2fe23226b1423bd2d7495
Evgeny Sinitsyn <jeka07_95@mail.ru>
2021-05-23 21:36:44 +0300

    L04-generics-homework

---
+++ L04-generics/src/main/java/homework/Customer.java
@@ -0,0 +1,66 @@
+package homework;
+
+public class Customer implements Cloneable {
+    private final long id;
+    private String name;
+    private long scores;
+
+    //todo: 1. в этом классе надо исправить ошибки
+
+    public Customer(long id, String name, long scores) {
+        this.id = id;
+        this.name = name;
+        this.scores = scores;
+    }
+
+    //Копирование обьекта
+    public Customer(Customer other) {
+        this(other.getId(), other.getName(), other.getScores());
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public long getScores() {
+        return scores;
+    }
+
+    public void setScores(long scores) {
+        this.scores = scores;
+    }
+
+    @Override
+    public String toString() {
+        return "Customer{" +
+                "id=" + id +
+                ", name=" + name +
+                ", scores=" + scores +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || this.getClass() != o.getClass()) return false;
+
+        Customer customer = (Customer) o;
+        /*if (this.id == customer.getId() && this.name.equals(customer.getName()) && this.scores == customer.getScores()) {
+            return true;
+        }*/
+        return this.id == customer.getId();
+    }
+
+    @Override
+    public int hashCode() {
+        return (int) (id);
+    }
+}
