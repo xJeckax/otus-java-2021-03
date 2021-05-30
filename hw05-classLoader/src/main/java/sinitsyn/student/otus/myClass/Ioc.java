26b086682103ade71ebbf2dbc34444a0ca64db6b
Evgeny Sinitsyn <jeka07_95@mail.ru>
2021-05-30 16:33:51 +0300

    hw05 - works with proxy

---
+++ hw05-classLoader/src/main/java/sinitsyn/student/otus/myClass/Ioc.java
@@ -0,0 +1,53 @@
+package sinitsyn.student.otus.myClass;
+
+import sinitsyn.student.otus.annotation.Log;
+
+import java.lang.annotation.Annotation;
+import java.lang.reflect.InvocationHandler;
+import java.lang.reflect.Method;
+import java.lang.reflect.Proxy;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+class Ioc {
+
+    private Ioc() {
+    }
+
+    static TerminatorFactory createMyClass() {
+        InvocationHandler handler = new DemoInvocationHandler(new Terminator());
+        return (TerminatorFactory) Proxy.newProxyInstance(TerminatorFactory.class.getClassLoader(),
+                new Class<?>[]{TerminatorFactory.class}, handler);
+    }
+
+    static class DemoInvocationHandler implements InvocationHandler {
+        private final TerminatorFactory myClass;
+
+        DemoInvocationHandler(TerminatorFactory myClass) {
+            this.myClass = myClass;
+        }
+
+        @Override
+        public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
+            System.out.println("Before method - " + method.getName());
+
+            Method meth = myClass.getClass().getMethod(method.getName(), method.getParameterTypes());
+            List<String> strings = Arrays.stream(args).map(String::valueOf).collect(Collectors.toList());
+            for (Annotation ann : meth.getDeclaredAnnotations()) {
+                if (ann instanceof Log) {
+                    System.out.printf("executed method : %s , params : %s%n", method.getName(),
+                            String.join(", ", strings));
+                }
+            }
+            return method.invoke(myClass, args);
+        }
+
+        @Override
+        public String toString() {
+            return "DemoInvocationHandler{" +
+                    "myClass=" + myClass +
+                    '}';
+        }
+    }
+}
---
+++ hw05-classLoader/src/main/java/sinitsyn/student/otus/myClass/TerminatorFactory.java
@@ -0,0 +1,8 @@
+package sinitsyn.student.otus.myClass;
+
+public interface TerminatorFactory {
+
+    void getQuote(String quote);
+
+    int getIdentityNumber();
+}
