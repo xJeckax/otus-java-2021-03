0cb334b6de709262a999d49d0dc6cfb633c670da
Evgeny Sinitsyn <jeka07_95@mail.ru>
2021-06-03 21:51:10 +0300

    исправление - добавления быстродействия

--- hw05-classLoader/src/main/java/sinitsyn/student/otus/myClass/Ioc.java
+++ hw05-classLoader/src/main/java/sinitsyn/student/otus/myClass/Ioc.java
@@ -2,10 +2,10 @@ package sinitsyn.student.otus.myClass;
 
 import sinitsyn.student.otus.annotation.Log;
 
-import java.lang.annotation.Annotation;
 import java.lang.reflect.InvocationHandler;
 import java.lang.reflect.Method;
 import java.lang.reflect.Proxy;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
@@ -23,22 +23,28 @@ class Ioc {
 
     static class DemoInvocationHandler implements InvocationHandler {
         private final TerminatorFactory myClass;
+        private final List<String> logging = new ArrayList<>();
 
         DemoInvocationHandler(TerminatorFactory myClass) {
             this.myClass = myClass;
+            Method[] methods = myClass.getClass().getDeclaredMethods();
+            for (Method meth : methods) {
+                Arrays.stream(meth.getDeclaredAnnotations()).forEach(annotation -> {
+                    if (annotation instanceof Log) {
+                        logging.add(meth.getName());
+                    }
+                });
+            }
         }
 
         @Override
         public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
             System.out.println("Before method - " + method.getName());
 
-            Method meth = myClass.getClass().getMethod(method.getName(), method.getParameterTypes());
-            List<String> strings = Arrays.stream(args).map(String::valueOf).collect(Collectors.toList());
-            for (Annotation ann : meth.getDeclaredAnnotations()) {
-                if (ann instanceof Log) {
-                    System.out.printf("executed method : %s , params : %s%n", method.getName(),
-                            String.join(", ", strings));
-                }
+            if (logging.contains(method.getName())) {
+                List<String> strings = Arrays.stream(args).map(String::valueOf).collect(Collectors.toList());
+                System.out.printf("executed method : %s , params : %s%n", method.getName(),
+                        String.join(", ", strings));
             }
             return method.invoke(myClass, args);
         }
